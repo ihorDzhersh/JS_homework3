331270ce3dfa356ffce5d42498d7848bea8e1d1c
ihorDzhersh <68298481+ihorDzhersh@users.noreply.github.com>
2020-07-21 22:02:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+//*********************task1**************************** */
+// let arr = [2, 3, 4, 5];
+// let mult = 1;
+// for (let i = 0; i < arr.length; i++) {
+//      mult *= arr[i];  
+// }
+// console.log(mult);
+
+
+// let arr = [2, 3, 4, 5];
+// let mult = 1;
+// let i = 0;
+// while(i<arr.length){
+//     mult *= arr[i]
+//     i++;
+// }
+// console.log(mult);
+
+
+//**************************task2 ******************** */
+
+// for(let i = 0; i<=15; i++){
+//     if((i%2) ==0)console.log(i + " - even");
+//     else (console.log(i+ " - odd"))
+// }
+
+//***************************task3****************** */
+
+
+// function getRandomArray(k){
+//     let newArr = [];
+//     for(let i =0; i < k; i++){
+//         newArr[i] = Math.floor((Math.random() * 500)+1);
+//     }
+//     console.log(newArr);
+// }
+
+// getRandomArray(5);
+
+//**************************task4*************** */
+
+
+// function raiseToDegree(a,b){
+//      console.log(Math.round((a**b)));    
+// }
+
+// raiseToDegree(3,4);
+
+
+//****************************task5**************** */
+// function findMin(...arguments){
+//    console.log(Math.min(...arguments));    
+// }
+
+// findMin(12, 14, 4, -4, 0.2);
+
+//****************************task6***************** */
+
+// function findUnique(arr){
+//    let isUnuque = arr.some((val, i) => arr.indexOf(val) !== i);
+//       return isUnuque ? console.log(false) : console.log(true);
+      
+// }
+
+// findUnique([1, 2, 3, 5, 3]);
+// findUnique([1, 2, 5, 3]);
+
+
+//або ще такий варіант
+
+// function findUnique(args) {
+
+//    let count = {};
+//    for(let i = 0; i < args.length; i++){
+//         count[args[i]] = 1 + (count[args[i]] || 0);
+//    }
+//    let found = Object.keys(count).filter(function(key) {
+//        return count[key] > 1;
+//    });
+//    return found.length ? console.log(false) : console.log(true); 
+// }
+
+// findUnique([1, 2, 3, 5, 3]);
+// findUnique([1, 2, 3, 5, 11]); 
+
+
+//************************task7****************** */
+
+
+// function getElements(arr,n){
+//   while(arr.length-n){
+//      arr.shift();
+//      if(arr.length <= 0){
+//         return arr;
+//      }
+//   }
+//   console.log(arr);
+  
+// }
+
+// getElements([1,2,3,4,5,78,8,9,6],3);
+ 
+
+//**********************task8 ********************* */
+// function titleCase(str) {
+//    let arr = str.toLowerCase().split(" ");
+//    let result = arr.map(function(val){
+//      return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+//    });
+//    let output =  result.join(" ");
+//    console.log(output);
+//  }
+ 
+// titleCase('i love java script');
+
